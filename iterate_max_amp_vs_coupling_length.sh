2f629472750b42b7e30526ac9e143aa5c66f4d85
danieljamesgraham <139626610+danieljamesgraham@users.noreply.github.com>
2024-10-30 18:40:29 +0000

    Changed iteration script parameters for long sim.

--- iterate_max_amp_vs_coupling_length.sh
+++ iterate_max_amp_vs_coupling_length.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-source .ring-resonator-coupling-simulation/bin/activate
+source .rr_sim/bin/activate
 for velocity_ratio in $(seq 1 0.1 2); do
 	echo "Current velocity ratio: $velocity_ratio";
-	python coupling_simulation.py max-amp-vs-coupling-length -i 1000 -d 1000 -e 0 -v 2.5 -r $velocity_ratio -e 1 --coupling-step 0.01 --max-coupling-length 6.5;
+	python coupling_simulation.py max-amp-vs-coupling-length -i 10000 -d 1000 -e 0 -v 2.5 -r $velocity_ratio -e 1 --coupling-step 0.001 --max-coupling-length 6.5;
 	echo "\n\n";
 done
